ed36d04d2108df21e1e27b133680d96ad90d0573
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2026-02-12 21:43:49 +0530

    add login and logout user controller

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -3,7 +3,7 @@ import { RequestHandler } from "express";
 
 import { generateToken } from "../utils/utils.js";
 import { User } from "../models/user.model.js";
-import { registerSchema } from "../vaidations/user.validation.js";
+import { loginSchema, registerSchema } from "../vaidations/user.validation.js";
 
 export const registerUser: RequestHandler = async (req, res) => {
   const parsed = registerSchema.safeParse(req.body);
@@ -39,4 +39,40 @@ export const registerUser: RequestHandler = async (req, res) => {
   return res.status(201).json({ user });
 };
 
-export const loginUser: RequestHandler = async (req, res) => {};
+export const loginUser: RequestHandler = async (req, res) => {
+  const parsed = loginSchema.safeParse(req.body);
+
+  if (!parsed.success) {
+    return res.status(400).json({ message: "Validation Failed" });
+  }
+
+  const { username, password } = parsed.data;
+
+  const user = await User.findOne({ username });
+
+  if (!user) {
+    return res.status(400).json({ message: "Invalid Credientials" });
+  }
+
+  const isPassowordValid = await bcrypt.compare(password, user.password);
+
+  if (!isPassowordValid) {
+    return res.status(400).json({ message: "Invalid Credientials" });
+  }
+
+  generateToken({ _id: user._id.toString() }, res);
+
+  return res.status(200).json(user);
+};
+
+export const logoutUser: RequestHandler = async (req, res) => {
+  return res
+    .status(200)
+    .cookie("jwt", "", {
+      maxAge: 0,
+      httpOnly: true,
+      sameSite: process.env.MODE !== "DEV" ? "none" : "lax",
+      secure: process.env.MODE !== "DEV",
+    })
+    .json({ message: "Logged out Successfully" });
+};
--- backend/src/routes/user.route.ts
+++ backend/src/routes/user.route.ts
@@ -1,9 +1,16 @@
 import { Router } from "express";
 import { asyncHandler } from "../utils/asyncHandler.js";
-import { registerUser } from "../controllers/user.controller.js";
+import {
+  loginUser,
+  logoutUser,
+  registerUser,
+} from "../controllers/user.controller.js";
+import { authMiddleware } from "../middlewares/auth.js";
 
 const router = Router();
 
 router.post("/register", asyncHandler(registerUser));
+router.post("/login", asyncHandler(loginUser));
+router.post("/logout", authMiddleware, asyncHandler(logoutUser));
 
 export default router;
