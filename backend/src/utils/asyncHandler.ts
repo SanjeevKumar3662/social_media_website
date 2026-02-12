3b9647b89c99cf65f488101755326456494d4f88
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2026-02-12 12:30:35 +0530

    add asyncHandler function

---
+++ backend/src/utils/asyncHandler.ts
@@ -0,0 +1,11 @@
+import { Request, Response, NextFunction, RequestHandler } from "express";
+
+export const asyncHandler =
+  (fn: RequestHandler): RequestHandler =>
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await fn(req, res, next);
+    } catch (error) {
+      next(error);
+    }
+  };
