64e4cb1682407fbf971396ba80e623a0f633705d
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2026-02-13 17:31:19 +0530

    update cors and ad route routers

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,8 +4,9 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 
 import { connectDB } from "./db/db.js";
-import userRouter from "../src/routes/user.route.js";
 import { errorMiddleware } from "./middlewares/errorMiddleware.js";
+import userRouter from "./routes/user.route.js";
+import postRouter from "./routes/post.route.js";
 
 const PORT = 3000;
 
@@ -14,9 +15,15 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(cookieParser());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
 
 app.use("/api/v1/user", userRouter);
+app.use("/api/v1/posts", postRouter);
 
 app.use(errorMiddleware());
 
