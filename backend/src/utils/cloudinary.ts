534f6ce0c54fb991503be738bd17da81f7c15c09
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2026-02-13 17:27:33 +0530

    add cloudinary config and uplodOnCloudinary function

---
+++ backend/src/utils/cloudinary.ts
@@ -0,0 +1,31 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+export const uploadOnCloudinary = async (localFilePath: string) => {
+  try {
+    if (!localFilePath) return null;
+
+    const uploadRes = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+
+    // delete temp file after success
+    fs.unlinkSync(localFilePath);
+
+    return uploadRes;
+  } catch (error) {
+    if (fs.existsSync(localFilePath)) {
+      fs.unlinkSync(localFilePath);
+    }
+
+    return null;
+  }
+};
+
+// export default cloudinary;
